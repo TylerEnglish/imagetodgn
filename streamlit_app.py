976a560109873d5f10bdb0da21aee6ced105b14c
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-08-21 08:40:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,33 +3,22 @@ from aspose.cad import Image
 from aspose.cad.imageoptions import CadRasterizationOptions, DgnOptions
 
 def convert_to_dgn(input_file, output_file):
-    # Load the image file
     image = Image.load(input_file)
-
-    # Set rasterization options
     rasterization_options = CadRasterizationOptions()
     rasterization_options.page_width = 1600
     rasterization_options.page_height = 1600
-
-    # Set DGN options
     dgn_options = DgnOptions()
     dgn_options.vector_rasterization_options = rasterization_options
-
-    # Save as DGN
     image.save(output_file, dgn_options)
 
 st.title("Image to DGN Converter")
-
 uploaded_file = st.file_uploader("Choose an image file", type=["svg", "png", "jpeg", "jpg"])
 
 if uploaded_file is not None:
     input_file = uploaded_file.name
     output_file = "output.dgn"
-
     with open(input_file, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
     convert_to_dgn(input_file, output_file)
-
     st.success("Conversion successful!")
     st.download_button("Download DGN file", data=open(output_file, "rb"), file_name=output_file)
