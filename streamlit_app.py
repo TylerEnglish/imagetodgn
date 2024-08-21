42e4f6a890b55139f090458d7c8554ee84dc1d7c
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-08-21 13:17:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,41 @@
 import streamlit as st
-from aspose.cad import Image
-from aspose.cad.imageoptions import CadRasterizationOptions, DgnOptions
+from aspose.cad import Color
+from aspose.cad import Image as CadImage
+from aspose.cad.imageoptions import DwgOptions, CadRasterizationOptions
 
-def convert_to_dgn(input_file, output_file):
-    image = Image.load(input_file)
-    rasterization_options = CadRasterizationOptions()
-    rasterization_options.page_width = 1600
-    rasterization_options.page_height = 1600
-    dgn_options = DgnOptions()
-    dgn_options.vector_rasterization_options = rasterization_options
-    image.save(output_file, dgn_options)
+def convert_to_dwg(img_file, dwg_output_file):
+    # Load the SVG file using Aspose.CAD
+    image = CadImage.load(img_file)
 
-st.title("Image to DGN Converter")
-uploaded_file = st.file_uploader("Choose an image file", type=["svg", "png", "jpeg", "jpg"])
+    cadRasterizationOptions = CadRasterizationOptions()
+    cadRasterizationOptions.page_height = image.height + .0
+    cadRasterizationOptions.page_width = image.width + .0
+    cadRasterizationOptions.zoom = 1.0
+    cadRasterizationOptions.background_color = Color.white
+    cadRasterizationOptions.no_scaling = True
+    # cadRasterizationOptions.automatic_layouts_scaling = False
+
+
+    dwg_options = DwgOptions()
+    dwg_options.vector_rasterization_options = cadRasterizationOptions
+    image.save(dwg_output_file, dwg_options)
+
+st.title("Image to DWG Converter")
+uploaded_file = st.file_uploader("Choose an PNG file", type=["png"])
 
 if uploaded_file is not None:
-    input_file = uploaded_file.name
-    output_file = "output.dgn"
-    with open(input_file, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    convert_to_dgn(input_file, output_file)
-    st.success("Conversion successful!")
-    st.download_button("Download DGN file", data=open(output_file, "rb"), file_name=output_file)
+    
+    
+    # Define the output DWG file path
+    dwg_output_file = "output.dwg"
+    
+    # Convert the SVG to DWG format
+    convert_to_dwg(uploaded_file, dwg_output_file)
+    
+    # Check if the DWG file was successfully created and has content
+    if os.path.exists(dwg_output_file) and os.path.getsize(dwg_output_file) > 0:
+        st.success("Conversion successful!")
+        with open(dwg_output_file, "rb") as file:
+            st.download_button("Download DWG file", data=file, file_name=dwg_output_file)
+    else:
+        st.error("Conversion failed or resulted in an empty DWG file.")
